b3df587b8354a6aa04cd1140c02fabc2aed80656
aliaspider <aliaspider@gmail.com>
2017-10-30 15:15:53 +0100

    misc

--- vulkan/shaders.h
+++ vulkan/shaders.h
@@ -1,16 +1,12 @@
 
-#define VS_SHADER_FILE xstr(SHADER_FILE.vert.inc)
-#define PS_SHADER_FILE xstr(SHADER_FILE.frag.inc)
-#define GS_SHADER_FILE xstr(SHADER_FILE.geom.inc)
-
 const uint32_t vs_code [] =
-#include VS_SHADER_FILE
+#include xstr(SHADER_FILE.vert.inc)
    ;
 const uint32_t ps_code [] =
-#include PS_SHADER_FILE
+#include xstr(SHADER_FILE.frag.inc)
    ;
 const uint32_t gs_code [] =
-#include GS_SHADER_FILE
+#include xstr(SHADER_FILE.geom.inc)
    ;
 
 #define SHADER_INFO \
