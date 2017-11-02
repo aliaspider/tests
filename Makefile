19eef2a9560dcd4f83a60c80049bc32e4b70996c
aliaspider <aliaspider@gmail.com>
2017-11-02 08:20:13 +0100

    misc

--- Makefile
+++ Makefile
@@ -34,7 +34,6 @@ OBJS += vulkan/font.o
 OBJS += vulkan/font_mono.o
 OBJS += vulkan/frame.o
 OBJS += vulkan/main.o
-OBJS += vulkan/resource.o
 OBJS += vulkan/slider.o
 OBJS += vulkan/sprite.o
 OBJS += vulkan/vulkan_common.o
---
+++ vulkan/common/resource.c
@@ -0,0 +1,73 @@
+
+#include "resource.h"
+#include "buffer.h"
+#include "texture.h"
+
+typedef struct vk_resource_t vk_resource_t;
+struct vk_resource_t
+{
+	bool dirty;
+	vk_resource_type_t type;
+	vk_resource_t *next;
+	vk_resource_t *prev;
+};
+
+static vk_resource_t *textures;
+static vk_resource_t *buffers;
+
+void vk_resource_add(void *ptr)
+{
+	vk_resource_t * resource = (vk_resource_t *)ptr;
+
+	if(resource->type == VK_RESOURCE_BUFFER && (((vk_buffer_t*)ptr)->mem.flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))
+		return;
+
+	vk_resource_t **last;
+	if(resource->type == VK_RESOURCE_TEXTURE)
+		last = &textures;
+	else
+		last = &buffers;
+
+	vk_resource_t * prev = NULL;
+	while (*last)
+	{
+		prev = *last;
+		last = &(*last)->next;
+	}
+
+	*last = resource;
+
+	resource->prev = prev;
+	resource->next = NULL;
+}
+
+void vk_resource_remove(void *ptr)
+{
+	vk_resource_t *resource = (vk_resource_t *)ptr;
+
+	if(resource->type == VK_RESOURCE_BUFFER && (((vk_buffer_t*)ptr)->mem.flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))
+		return;
+
+	if (resource->next)
+		resource->next->prev = resource->prev;
+
+	if (resource->prev)
+		resource->prev->next = resource->next;
+	else
+	{
+		if(resource->type == VK_RESOURCE_TEXTURE)
+			textures = resource->next;
+		else
+			buffers = resource->next;
+	}
+
+}
+
+void vk_resource_flush_all(VkDevice device, VkCommandBuffer cmd)
+{
+	for(vk_resource_t* resource = textures; resource; resource = resource->next)
+		vk_texture_flush(device, cmd, (vk_texture_t*)resource);
+
+	for(vk_resource_t* resource = buffers; resource; resource = resource->next)
+		vk_buffer_flush(device, (vk_buffer_t*)resource);
+}
